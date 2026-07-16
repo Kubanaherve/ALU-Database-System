8e2567093ace8bb0188bf609719a441ca4ac3416
Balinda55 <s.balinda1@alustudent.com>
2026-07-16 15:48:47 +0200

    Add Courses table

--- alu_database.sql
+++ alu_database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE NESA;
+IICREATE DATABASE NESA;
 USE NESA;
 
 
@@ -61,13 +61,14 @@ USE NESA;
    SECTION 4 - COURSES (Sonia)
    Branch: course-sonia
    */
-
-
-
-
-
-
-
+CREATE TABLE Courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    course_code VARCHAR(20) UNIQUE NOT NULL,
+    credits INT NOT NULL,
+    faculty_id INT,
+    FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id)
+);
 
 
 
