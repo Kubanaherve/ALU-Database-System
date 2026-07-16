a7352318e2cb7591f30fdab8652683d6390dc9e5
marvellio77 <m.munezero@alustudent.com>
2026-07-16 15:54:52 +0200

    Add Student_Courses and Student_Activities junction tables

--- alu_database.sql
+++ alu_database.sql
@@ -94,22 +94,45 @@ USE NESA;
    Branch: junctions-marvella
   */
 
+CREATE TABLE Student_Courses (
+    student_id INT,
+    course_id INT,
 
+    PRIMARY KEY (student_id, course_id),
 
+    FOREIGN KEY (student_id)
+        REFERENCES Students(student_id),
 
+    FOREIGN KEY (course_id)
+        REFERENCES Courses(course_id)
+);
 
+INSERT INTO Student_Courses VALUES
+(1,1),
+(1,2),
+(2,1),
+(3,3),
+(4,2);
 
+CREATE TABLE Student_Activities (
+    student_id INT,
+    activity_id INT,
 
+    PRIMARY KEY (student_id, activity_id),
 
+    FOREIGN KEY (student_id)
+        REFERENCES Students(student_id),
 
+    FOREIGN KEY (activity_id)
+        REFERENCES Extra_Curricular_Activities(activity_id)
+);
 
-
-
-
-
-
-
-
+INSERT INTO Student_Activities VALUES
+(1,1),
+(2,2),
+(3,1),
+(4,3),
+(5,2)
 
 /* 
    SECTION 7 - QUERIES (Friend)
